4b7b66c096b7f897ec7542599922f04f45b07deb
Viktor Kabachenko <vkabachenko@gmail.com>
2019-07-11 11:29:11 +0300

    format comments

--- views/bid-comment/_comment.php
+++ views/bid-comment/_comment.php
@@ -2,14 +2,25 @@
 /* @var $model \app\models\BidComment */
 ?>
 
-<div style="margin: 0 10px;">
-    <div>
-        Дата создания <?= $model->created_at ?>
+<div>
+    <div class="col-sm-4 col-xs-12">
+        <div class="col-sm-12 col-xs-6">
+            <?= $model->created_at ?>
+        </div>
+        <div  class="col-sm-12 col-xs-6">
+            <?= $model->user->username ?>
+        </div>
     </div>
-    <div>
-        Комментатор <?= $model->user->username ?>
+    <div class="col-sm-8 col-xs-12">
+        <div style="min-height: 50px; max-height: 300px; overflow: auto; border: solid 1px #aaa; padding: 5px;">
+            <?= $model->comment ?>
+        </div>
     </div>
-    <div style="min-height: 50px; max-height: 300px; overflow: auto; border: solid 1px #aaa; padding: 5px;">
-        <?= $model->comment ?>
-    </div>
+    <div class="clearfix"></div>
+<div>
+    <hr style="color: #bbb;">
+</div>
+
+
+
 </div>
