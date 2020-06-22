25f9e7e573ab344dbb144a28c1196d893756b5e7
viktor <vkabachenko@gmail.com>
2020-06-22 10:24:01 +0300

    set num order fix out of memory bug

--- commands/DbController.php
+++ commands/DbController.php
@@ -14,40 +14,33 @@ class DbController extends Controller
 {
     public function actionSetNumOrder()
     {
-        $bids = Bid::find()->all();
+        $spares = Spare::find()->where(['num_order' => 0])-> all();
 
-        foreach ($bids as $bid) {
-            /* @var $bid Bid */
-            echo $bid->id . "\n";
-
-            $maxNumOrder = Spare::find()->where(['bid_id' => $bid->id])->max('num_order');
-            foreach ($bid->spares as $spare) {
-                /* @var $spare Spare */
-                if ($spare->num_order === 0) {
-                    $spare->save();
-                }
-            }
-
-            $maxNumOrder = ReplacementPart::find()->where(['bid_id' => $bid->id])->max('num_order');
-            foreach ($bid->replacementParts as $replacementPart) {
-                /* @var $replacementPart ReplacementPart */
-                if ($replacementPart->num_order === 0) {
-                    $maxNumOrder++;
-                    $replacementPart->num_order = $maxNumOrder;
-                    $replacementPart->save();
-                }
-            }
-
-            $maxNumOrder = ClientProposition::find()->where(['bid_id' => $bid->id])->max('num_order');
-            foreach ($bid->clientPropositions as $clientProposition) {
-                /* @var $clientProposition ClientProposition */
-                if ($clientProposition->num_order === 0) {
-                    $maxNumOrder++;
-                    $clientProposition->num_order = $maxNumOrder;
-                    $clientProposition->save();
-                }
-            }
+        foreach ($spares as $spare) {
+                $spare->save();
         }
+
+        echo 'spares' . "\n";
+
+        $replacementParts = ReplacementPart::find()->where(['num_order' => 0])-> all();
+
+        foreach ($replacementParts as $replacementPart) {
+            $maxNumOrder = ReplacementPart::find()->where(['bid_id' => $replacementPart->bid_id])->max('num_order');
+            $replacementPart->num_order = $maxNumOrder + 1;
+            $replacementPart->save();
+        }
+
+        echo 'replacementParts' . "\n";
+
+        $clientPropositions = ClientProposition::find()->where(['num_order' => 0])-> all();
+
+        foreach ($clientPropositions as $clientProposition) {
+            $maxNumOrder = ClientProposition::find()->where(['bid_id' => $clientProposition->bid_id])->max('num_order');
+            $clientProposition->num_order = $maxNumOrder + 1;
+            $clientProposition->save();
+        }
+
+        echo 'clientPropositions' . "\n";
     }
 
 }
